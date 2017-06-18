529881c458253250dbb4d0447dcc758ba05bd99e
Tom Prince <tom.prince@twistedmatrix.com>
2017-06-17 22:15:18 -0600

    Build base image first.

--- run-tc.sh
+++ run-tc.sh
@@ -11,12 +11,23 @@ df -h;
 du . | sort -nr | head -n100
 
 src/ci/init_repo.sh . "$HOME/rustsrc"
+
+if grep -q "FROM rust-ci-base" "src/ci/docker/$IMAGE/Dockerfile"; then
+    docker \
+      build \
+      --rm \
+      -t rust-ci-base \
+      -f "src/ci/docker/base/Dockerfile" \
+      "src/ci/docker"
+fi
+
 docker \
     build \
     --rm \
     -t rust-ci \
     -f "src/ci/docker/$IMAGE/Dockerfile" \
     "src/ci/docker"
+
 docker \
     save \
     -o "image.tar" \
